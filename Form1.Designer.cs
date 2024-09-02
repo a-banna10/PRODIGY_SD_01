b29b59134aa5c2397f8c6ab4ed9bb382e6d271ad
LENOVO <LENOVO@DESKTOP-1Q7JF0S>
2024-09-02 17:06:14 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace PRODIGY_SD_01
+{
+    partial class TemperatureConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TemperatureConverter));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.cmbUnit = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            this.lblCelsius = new System.Windows.Forms.Label();
+            this.lblKelvin = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PowderBlue;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(158, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(374, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature Converter";
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTemperature.ForeColor = System.Drawing.Color.Black;
+            this.txtTemperature.Location = new System.Drawing.Point(268, 111);
+            this.txtTemperature.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(92, 27);
+            this.txtTemperature.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(202, 190);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 24);
+            this.label8.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(244, 135);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 24);
+            this.label7.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(244, 105);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 24);
+            this.label6.TabIndex = 18;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnConvert.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(268, 307);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(91, 37);
+            this.btnConvert.TabIndex = 17;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // cmbUnit
+            // 
+            this.cmbUnit.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbUnit.ForeColor = System.Drawing.Color.Black;
+            this.cmbUnit.FormattingEnabled = true;
+            this.cmbUnit.Location = new System.Drawing.Point(268, 140);
+            this.cmbUnit.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.cmbUnit.Name = "cmbUnit";
+            this.cmbUnit.Size = new System.Drawing.Size(92, 30);
+            this.cmbUnit.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(154, 256);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 24);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Kelvin:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(154, 200);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Celsius:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(154, 227);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Fahrenheit:";
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnit.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblUnit.Location = new System.Drawing.Point(155, 139);
+            this.lblUnit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(46, 24);
+            this.lblUnit.TabIndex = 12;
+            this.lblUnit.Text = "Unit:";
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperature.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTemperature.Location = new System.Drawing.Point(155, 112);
+            this.lblTemperature.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(112, 24);
+            this.lblTemperature.TabIndex = 11;
+            this.lblTemperature.Text = "Temperature:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(429, 105);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 219);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFahrenheit.Location = new System.Drawing.Point(262, 227);
+            this.lblFahrenheit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(0, 22);
+            this.lblFahrenheit.TabIndex = 23;
+            // 
+            // lblCelsius
+            // 
+            this.lblCelsius.AutoSize = true;
+            this.lblCelsius.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCelsius.Location = new System.Drawing.Point(262, 200);
+            this.lblCelsius.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCelsius.Name = "lblCelsius";
+            this.lblCelsius.Size = new System.Drawing.Size(0, 22);
+            this.lblCelsius.TabIndex = 24;
+            // 
+            // lblKelvin
+            // 
+            this.lblKelvin.AutoSize = true;
+            this.lblKelvin.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKelvin.Location = new System.Drawing.Point(262, 256);
+            this.lblKelvin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblKelvin.Name = "lblKelvin";
+            this.lblKelvin.Size = new System.Drawing.Size(0, 22);
+            this.lblKelvin.TabIndex = 25;
+            // 
+            // TemperatureConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(741, 450);
+            this.Controls.Add(this.lblKelvin);
+            this.Controls.Add(this.lblCelsius);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.cmbUnit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblUnit);
+            this.Controls.Add(this.lblTemperature);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "TemperatureConverter";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TemperatureConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ComboBox cmbUnit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.Label lblTemperature;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblFahrenheit;
+        private System.Windows.Forms.Label lblCelsius;
+        private System.Windows.Forms.Label lblKelvin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PRODIGY_SD_01
+{
+    public partial class TemperatureConverter : Form
+    {
+        public TemperatureConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void TemperatureConverter_Load(object sender, EventArgs e)
+        {
+            cmbUnit.Items.Add("Celsius");
+            cmbUnit.Items.Add("Fahrenheit");
+            cmbUnit.Items.Add("Kelvin");
+            cmbUnit.SelectedIndex = 0;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double temperature = Convert.ToDouble(txtTemperature.Text);
+                string unit = cmbUnit.SelectedItem.ToString();
+                double fahrenheit, celsius, kelvin;
+                switch (unit)
+                {
+                    case "Celsius":
+                        celsius = temperature;
+                        fahrenheit = celsius * 9 / 5 + 32;
+                        kelvin = celsius + 273.15;
+                        lblCelsius.Text = celsius.ToString() + "°C";
+                        lblFahrenheit.Text = fahrenheit.ToString() + "°F";
+                        lblKelvin.Text = kelvin.ToString() + "K";
+                        break;
+
+                    case "Fahrenheit":
+                        fahrenheit = temperature;
+                        celsius = (fahrenheit - 32) * 5 / 9;
+                        kelvin = celsius + 273.15;
+                        lblCelsius.Text = celsius.ToString() + "°C";
+                        lblFahrenheit.Text = fahrenheit.ToString() + "°F";
+                        lblKelvin.Text = kelvin.ToString() + "K";
+                        break;
+
+                    case "Kelvin":
+                        kelvin = temperature;
+                        celsius = kelvin - 273.15;
+                        fahrenheit = celsius * 9 / 5 + 32;
+                        lblCelsius.Text = celsius.ToString() + "°C";
+                        lblFahrenheit.Text = fahrenheit.ToString() + "°F";
+                        lblKelvin.Text = kelvin.ToString() + "K";
+                        break;
+                }
+            }
+            catch (FormatException) { MessageBox.Show("Invalid input! Try again"); }
+
+        }
+    }
+}
